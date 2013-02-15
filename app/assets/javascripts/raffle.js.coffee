6a50b0fb818b2c4d9f39bc986c10637c1885322d
Michael Hinrichs <michaelghinrichs@gmail.com>
2013-02-15 15:50:00 -0800

    first commit

---
+++ app/assets/javascripts/raffle.js.coffee
@@ -0,0 +1,19 @@
+@RaffleCtrl = ($scope) ->
+  $scope.entries = [
+    {name: 'Larry'}
+    {name: 'Curly'}
+    {name: 'Moe'}
+  ]
+
+  $scope.addEntry = ->
+    $scope.entries.push($scope.newEntry)
+    $scope.newEntry = {}
+
+  $scope.drawWinner = ->
+    pool = []
+    angular.forEach $scope.entries, (entry) ->
+      pool.push(entry) if !entry.winner
+    if pool.length > 0
+      entry = pool[Math.floor(Math.random()*pool.length)]
+      entry.winner = true
+      $scope.lastWinner = entry
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '369033c5c65c2b9e5ae9e13b35a338819e646c221482efbe562ea59d7de07404db9b465e83d37bcd6f107857bbcc9595a75a02c46e94986966be92099fa4ad9c'
